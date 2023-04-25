ce440273926a9cc335790acd3177149f12d5e97a
silent.mo <silent.mo@lianwei.com.cn>
2023-04-25 15:45:04 +0800

    add example test

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
---
+++ prometheus/example_test.go
@@ -0,0 +1,85 @@
+// Copyright © 2023  silent mo <1916393131@qq.com>.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package prometheus
+
+import (
+	"context"
+	"fmt"
+	"github.com/mo-silent/go-devops"
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
+func ExamplePrometheus_Push() {
+	// prometheus push metrics example
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+	p := devops.NewDevops().Prometheus()
+	// PushMetrics is prometheus.PushMetrics.
+	// The example is in the same directory as the structure,
+	// so it does not have a directory beginning
+	pm := PushMetrics{
+		Name:  "test",
+		Label: []string{"sample1", "sample2"},
+		// ProMetrics is prometheus.ProMetrics
+		Metrics: []PromMetrics{
+			{
+				Values: []string{"s1", "s2"},
+				Data:   99.99,
+			},
+		},
+	}
+	if err := p.Push(ctx, pm, "localhost:9091"); err != nil {
+		log.Errorf("push metrics error, err:  %v", err)
+	}
+}
+
+func ExamplePrometheus_QueryRange() {
+	// An example of how to query Prometheus metrics by range.
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	// new prometheus client
+	client, err := api.NewClient(api.Config{
+		Address: "http://localhost:80",
+	})
+	if err != nil {
+		log.Errorf("Error creating client: %v\n", err)
+		return
+	}
+
+	end := time.Now().UTC()
+
+	r := v1.Range{
+		Start: end.Add(-4 * time.Minute),
+		End:   end,
+		Step:  time.Minute,
+	}
+	log.Debugf("start: %v, end time: %v", end.Add(-6*time.Minute), end)
+
+	p := devops.NewDevops().Prometheus()
+	// WithTimeout() is prometheus.WithTimeout()
+	res, err := p.QueryRange(ctx, client, "test", r, WithTimeout(5*time.Second))
+	if err != nil {
+		log.Errorf("Error querying Prometheus: %v\n", err)
+	}
+	fmt.Println(res)
+}
